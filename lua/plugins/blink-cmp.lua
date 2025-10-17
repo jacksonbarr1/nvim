e6559280c5ed6cee65924827c173f281430ef988
Jackson Barr <jacksonbarr2021@gmail.com>
2025-10-17 18:30:54 -0400

    Fix blink config

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,8 +4,16 @@ return {
 	version = "1.*",
 	opts = {
 		keymap = { preset = "default" },
-	},
-	sources = {
-		default = { "lsp", "path", "snippets", "buffer" },
+		completion = {
+			menu = { border = "rounded" },
+			documentation = { window = { border = "rounded" }, auto_show = true },
+		},
+		signature = { window = { border = "rounded" }, enabled = true },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 	},
 }
