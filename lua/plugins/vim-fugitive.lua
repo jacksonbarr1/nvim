3c056797dc01acec363a8b77384dda17dcaaadd6
Jackson Barr <jacksonbarr2021@gmail.com>
2025-10-17 11:49:36 -0400

    Add push keybind

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -3,7 +3,8 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>gs", ":Git<CR>", { noremap = true, desc = "git status" })
     vim.keymap.set("n", "<leader>ga", ":Git add ", { noremap = true, desc = "git add" })
-    vim.keymap.set('n','<leader>gA',':Git add .<CR>', {noremap = true, desc ='git add .' })
-    vim.keymap.set('n','<leader>gc',':Git commit -qam "',{noremap = true, desc = 'git commit -am' })
+    vim.keymap.set('n', '<leader>gA',':Git add .<CR>', {noremap = true, desc ='git add .' })
+    vim.keymap.set('n', '<leader>gc',':Git commit -qam "',{noremap = true, desc = 'git commit -am' })
+    vim.keymap.set("n", "<leader>gp", ":Git push <CR>", { noremap = true, desc = "git push" })
   end
 }
