c8a7ee56d5ec9be67a67e4d519cfc52961697a6a
Jackson Barr <jacksonbarr2021@gmail.com>
2025-10-17 14:51:53 -0400

    REVERT

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 require("config.options")
-require("core.lsp")
 require("config.keymaps")
-require("config.autocmds")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
