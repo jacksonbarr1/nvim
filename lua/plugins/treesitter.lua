e1c2dd67b6112be2e208dec86432022c200cb35f
Jackson Barr <jacksonbarr2021@gmail.com>
2025-10-17 11:58:45 -0400

    Add config to treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy  = "false",
     build = ":TSUpdate",
     config = function()
       local config = require("nvim-treesitter.configs")
