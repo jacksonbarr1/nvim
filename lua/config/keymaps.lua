77fc0097ab9ff9b4e55d9b5724eaf5f408ecddf3
Jackson Barr <jacksonbarr2021@gmail.com>
2025-10-17 21:36:29 -0400

    Switch from telescope to fzf-lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,7 @@ vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set("n", "<leader>L", "<cmd>Lazy<CR>", { desc = "Lazy" })
+
+vim.keymap.set("n", "<leader>R", "<cmd>restart<CR>", { desc = "Restart Neovim" })
